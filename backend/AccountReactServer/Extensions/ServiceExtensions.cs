ea1c9f3c8aed454a738e06543a7e9ee1ed5fe16e
KauanMP <kauanmoreira1232@gmail.com>
2023-08-16 17:28:27 -0300

    Topico 22
    Adicionando a regra de cors na program para que possa ser utilizado; Adicionando Interface do loggerManager; criando uma referencia das pastas contracts e loggerManager para a accountReactServer e adicionando a biblioteca de logs 'Nlog'

--- backend/AccountReactServer/Extensions/ServiceExtensions.cs
+++ backend/AccountReactServer/Extensions/ServiceExtensions.cs
@@ -5,6 +5,7 @@ namespace AccountReactServer.Extensions
     {
         public static void ConfigureCors(this IServiceCollection services)
         {
+            // Configuração do cors
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
@@ -13,6 +14,7 @@ namespace AccountReactServer.Extensions
             });
         }
 
+        // Configura as opções de IIS (Internet Information Services) usando o IISOptions
         public static void ConfigureIISIntegration(this IServiceCollection services) {
             services.Configure<IISOptions>(options => {});
         }
--- backend/AccountReactServer/Program.cs
+++ backend/AccountReactServer/Program.cs
@@ -1,6 +1,11 @@
+using AccountReactServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,9 +19,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+    app.UseDeveloperExceptionPage();
+} else 
+    app.UseHsts();
+
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
